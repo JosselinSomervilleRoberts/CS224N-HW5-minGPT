fbb6b53a817f4deeb2f6a3df2a07bb7020ded941
JosselinSomervilleRoberts <josselin.somerville@polytechnique.edu>
2023-02-12 00:14:51 +0100

    Debug corpus

--- src/run.py
+++ src/run.py
@@ -152,6 +152,7 @@ elif args.function == 'finetune':
 
     # Step 2: Finetune the model
     corpus = open(args.finetune_corpus_path, encoding='utf-8').read()
+    print(corpus)
     finetune_dataset = dataset.NameDataset(pretrain_dataset, corpus)
     tconf = trainer.TrainerConfig(max_epochs=75,
                                     batch_size=256,
